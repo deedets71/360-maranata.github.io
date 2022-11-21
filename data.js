df81555a194e0f8e7772daa94161622de1210309
deedets71@gmail.com <deedets71@gmail.com>
2022-11-22 03:09:54 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,293 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-main-hall-lobby-view-1",
+      "name": "1 MAIN HALL LOBBY VIEW 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 2.1276160808495845,
+        "pitch": 0.06365355324779642,
+        "fov": 0.5181573956192993
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4824422747753738,
+          "pitch": 0.20806826423494051,
+          "rotation": 0,
+          "target": "1-2-main-hall-lobby-view-2"
+        },
+        {
+          "yaw": 0.277033038971485,
+          "pitch": 0.1703843365261406,
+          "rotation": 0,
+          "target": "2-3-auditorium"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2-main-hall-lobby-view-2",
+      "name": "2 MAIN HALL LOBBY VIEW 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.5529951542156333,
+        "pitch": 0.013671813552573298,
+        "fov": 0.5533486643119866
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6975276233949934,
+          "pitch": 0.20525693094121777,
+          "rotation": 0,
+          "target": "0-1-main-hall-lobby-view-1"
+        },
+        {
+          "yaw": -0.5247850653003283,
+          "pitch": 0.16351935670683204,
+          "rotation": 0,
+          "target": "2-3-auditorium"
+        },
+        {
+          "yaw": 0.7613519833372564,
+          "pitch": 0.14020478185299545,
+          "rotation": 0,
+          "target": "3-4-gallery-exterior-view-0"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3-auditorium",
+      "name": "3 AUDITORIUM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.7929598136769371,
+        "pitch": -0.0030433738655624154,
+        "fov": 0.9722120360624414
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5016100631805518,
+          "pitch": -0.21455368741460745,
+          "rotation": 0,
+          "target": "0-1-main-hall-lobby-view-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4-gallery-exterior-view-0",
+      "name": "4 GALLERY EXTERIOR VIEW 0",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -3.073348462837874,
+        "pitch": 0.009062483871595362,
+        "fov": 1.0147281054462685
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1251207904011196,
+          "pitch": 0.18361472465455897,
+          "rotation": 0,
+          "target": "4-5-gallery-1"
+        },
+        {
+          "yaw": -2.0643377399093374,
+          "pitch": 0.21899968691881,
+          "rotation": 0,
+          "target": "1-2-main-hall-lobby-view-2"
+        },
+        {
+          "yaw": 2.235122774688458,
+          "pitch": 0.16278055454530183,
+          "rotation": 0,
+          "target": "5-6-gallery-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-5-gallery-1",
+      "name": "5 GALLERY 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 3.1285284949409116,
+        "pitch": -0.008821484550487924,
+        "fov": 0.8480420921433199
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.07919599325468774,
+          "pitch": 0.1532525482150362,
+          "rotation": 0,
+          "target": "3-4-gallery-exterior-view-0"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-6-gallery-2",
+      "name": "6 GALLERY 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 3.125961144211889,
+        "pitch": 0.07501936258134378,
+        "fov": 0.8847759464616605
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04356479185192086,
+          "pitch": 0.34762375598226214,
+          "rotation": 0,
+          "target": "3-4-gallery-exterior-view-0"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "360 MARANATA",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
